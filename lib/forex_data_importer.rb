1457862c6e6ddf79e41525ce1df2b320e3bc71a6
Shivasubramanian A <shivasa@thoughtworks.com>
2012-01-02 18:21:42 +0530

    [Shiva/Ponnulingam]: Changed log message.

--- lib/forex_data_importer.rb
+++ lib/forex_data_importer.rb
@@ -6,15 +6,18 @@ class ForexDataImporter
   def import(from_file)
     read_from_excel(from_file, 3) do |extractor|
       begin
-      ForexPayment.create(issue_date: extractor.call("B"),
-                          emp_id: extractor.call("C"), emp_name: extractor.call("D"),
-                          amount: extractor.call("E"), currency: extractor.call("F"),
-                          travel_date: extractor.call("G"), place: extractor.call("H"),
-                          project: extractor.call("I"), vendor_name: extractor.call("J"),
-                          card_number: extractor.call("K"), expiry_date: extractor.call("L"),
-                          office: extractor.call("M"), inr: extractor.call("N"))
+        ForexPayment.create(issue_date: extractor.call("B"),
+                            emp_id: extractor.call("C"), emp_name: extractor.call("D"),
+                            amount: extractor.call("E"), currency: extractor.call("F"),
+                            travel_date: extractor.call("G"), place: extractor.call("H"),
+                            project: extractor.call("I"), vendor_name: extractor.call("J"),
+                            card_number: extractor.call("K"), expiry_date: extractor.call("L"),
+                            office: extractor.call("M"), inr: extractor.call("N"))
       rescue
-        puts 'Error while processing - todo need fix this'
+        puts "Error while processing the record:
+        Employee Name: #{extractor.call('D') }
+        Issue Date: #{extractor.call('B')}
+        Amount: #{extractor.call('E')} "
       end
 
     end
