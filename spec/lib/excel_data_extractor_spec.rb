2ab401535596353ef339e514e947ec04648a4150
Prasanna <pvenkat4ever@gmail.com>
2012-01-03 17:06:08 +0530

    Prasanna/Ashok: Added a spec for the excel parser

---
+++ spec/lib/excel_data_extractor_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+require 'excel_data_extractor'
+
+describe "Excel data extractor" do
+
+include ExcelDataExtractor
+
+  it "should extract details from excel" do
+    file_name = "file_name"
+    sheet_id = 0
+    callback = lambda{|extractor| extractor.call}
+    file_mock = mock("excel_file", :sheets => [mock("sheet")], :last_row => 3, :cell => "cell", :default_sheet= => "nothing")
+    should_receive(:handler).with(file_name).and_return(file_mock)
+    file_mock.should_receive(:cell).twice
+    read_from_excel(file_name, sheet_id, &callback)
+  end
+end
