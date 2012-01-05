8756c02334af7a229496401da4736dcfec707173
Ashok <ashokk@thoughtworks.com>
2012-01-05 18:03:46 +0530

    Ashok/Isha - Added custom to_json to profile

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,6 @@
 class Profile < ActiveRecord::Base
-
+  
+  def to_json
+    {:id => employee_id, :name => name}
+  end
 end
---
+++ spec/models/profile_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe 'profile' do
+
+  it "profile should display only id and name when converted to json" do
+    profile = Profile.new(:name => 'test', :employee_id => '1234')
+    profile.to_json.should == {:id=>"1234", :name=>"test"}
+  end
+end
