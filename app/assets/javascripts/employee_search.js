f98aa5994194f95663e98883f5fb163f6c8cbd19
isha <isha@isha-Latitude-E6510.(none)>
2012-01-06 16:26:27 +0530

    Isha/Ashok - added autosearch for emp id in forex payment

--- app/assets/javascripts/employee_search.js
+++ app/assets/javascripts/employee_search.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 $('.emp_name').autocomplete({
-   source: '/profiles/list', 
+   source: '/profiles/search_by_name', 
    minLength: 2,
    select: function(event, ui) {
      $('.no_emp').hide();
@@ -9,10 +9,25 @@ $('.emp_name').autocomplete({
      $('.emp_id').val(name_id[1]);
      return false;
    },
-   change: function(event, ui) {
+       change: function(event, ui) {
      $('.emp_id').val('');
      $('.no_emp').show();
 
-   },
+   }
+})
+    $('.emp_id').autocomplete({
+    source: '/profiles/search_by_id',
+    minLength: 2,
+    select: function(event, ui) {
+        $('.no_emp').hide();
+        name_id = ui.item.value.split('-');
+        $('.emp_id').val(name_id[1]);
+        $('.emp_name').val(name_id[0]);
+        return false;
+        },
+    change: function(event, ui) {
+    $('.emp_names').val('');
+    $('.no_emp').show();
+   }
 })
 });
--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,11 @@
 class ProfilesController < ApplicationController
-  def list
+  def search_by_name
     @profiles = Profile.where("name like '#{params[:term]}%'")
+    render :text => @profiles.collect(&:to_special_s)
+  end
+
+  def search_by_id
+    @profiles = Profile.where("employee_id like '#{params[:term]}%'")
     render :text => @profiles.collect(&:to_special_s)
   end
 end
