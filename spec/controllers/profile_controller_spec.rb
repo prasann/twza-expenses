180d7d288e600532b2ea1d777ab7fef531c04654
Ashok <ashokk@thoughtworks.com>
2012-01-06 10:34:42 +0530

    Ashok - Fixed a test

--- spec/controllers/profile_controller_spec.rb
+++ spec/controllers/profile_controller_spec.rb
@@ -5,6 +5,7 @@ describe ProfileController do
   it "should fetch the profile for the given name using like" do
     profiles = Profile.new
     Profile.should_receive(:where).with("name like 'test%'").and_return(profiles)
+    profiles.should_receive(:to_json).and_return({:id => '123', :name => 'test'})
     get :list, :name => 'test'
     assigns(:profiles).should be profiles
     response.should be_success
