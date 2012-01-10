35bc9f6dc5b28913c7138f78b442490383f00515
rajivra <rajiv@localhost>
2012-01-10 10:02:11 +0530

    Rajiv: added spec for consolidation

--- spec/models/expense_report_spec.rb
+++ spec/models/expense_report_spec.rb
@@ -10,7 +10,9 @@ describe 'expense_report' do
 								Expense.new(expense_rpt_id: '121',original_currency: 'EUR', original_cost:'100'),
 								Expense.new(expense_rpt_id: '121',original_currency: 'EUR', original_cost:'200')
 							 ]
+		forex_payments = [ForexPayment.new(:currency => 'EUR', :inr => 100, :amount => 100)] 
 		Expense.stub(:find).and_return(persisted_expenses)
+		ForexPayment.stub(:find).and_return(forex_payments)
 		exp_rpt = ExpenseReport.new(:expenses => ['somehashes'])
 		expected_hash_1 = {'report_id' => '123', 'currency' => 'EUR', 'amount' => 200, 'conversion_rate' => 1, 'local_currency_amount' => 200} 
 		expected_hash_2 = {'report_id' => '122', 'currency' => 'EUR', 'amount' => 100, 'conversion_rate' => 1, 'local_currency_amount' => 100} 
@@ -24,6 +26,21 @@ describe 'expense_report' do
 		actual_consolidated.should include(expected_hash_3)
 		actual_consolidated.should include(expected_hash_4)
 		
+	end
+
+	it "should provide average forex rate for each currency" do
+		forex_payments = [ForexPayment.new(:currency => 'EUR', :inr => 2000, :amount => 100), 
+						ForexPayment.new(:currency => 'EUR', :inr => 1800, :amount => 100),
+						ForexPayment.new(:currency => 'EUR', :inr => 950, :amount => 50),
+						ForexPayment.new(:currency => 'USD', :inr => 1000, :amount => 100),
+						ForexPayment.new(:currency => 'USD', :inr => 810, :amount => 90)]
+
+		ForexPayment.stub!(:find).and_return(forex_payments)
+		exp_rpt = ExpenseReport.new(:forex_payments => ['id1', 'id2'])
+		actual_conv_rates = exp_rpt.get_conversion_rates_for_currency()
+		actual_conv_rates.values.count == 2
+		actual_conv_rates["USD"].should == 9.50
+		actual_conv_rates["EUR"].should == 19.00
 	end
 
 end
