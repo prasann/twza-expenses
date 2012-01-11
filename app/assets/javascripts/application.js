60f183630df2bc30775c3b3d1c06cb46493e3480
Prasanna <pvenkat4ever@gmail.com>
2012-01-11 15:32:41 +0530

    Prasanna/Isha : Added accordian to the expense report

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,5 +23,21 @@ $(document).ready(function(){
    				 if(ui.index == 1){ window.location = "/forex_payments#forex_payments";} 
   			}
 		});
-	});       
+	});  
+	
+	$(function($){
+		$('#expense_report_accordion').accordion({
+			active: false,
+			autoHeight: false,
+    		collapsible: true
+		});
+
+		$("#expense_report_accordion h2 input").click(function(evt) { 
+        	evt.stopPropagation()
+        	if($(this).attr('checked') == 'checked')
+        		$('#'+$(this).attr('name')+'_table').find($('input')).attr('checked','checked')
+        	else
+        		$('#'+$(this).attr('name')+'_table').find($('input')).removeAttr('checked')	
+    	}); 
+	})     
 });
