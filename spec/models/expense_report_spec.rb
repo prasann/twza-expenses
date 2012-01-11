c45a685989fc27143c3ef1b5f555314cb2d5f828
Praveena G <praveena.gunasekhar@gmail.com>
2012-01-11 17:06:57 +0530

    Praveena/Rajiv : Added conversion rate logic for generating report with multiple currency

--- spec/models/expense_report_spec.rb
+++ spec/models/expense_report_spec.rb
@@ -2,13 +2,13 @@ require 'spec_helper'
 
 describe 'expense_report' do
 
-	it "should consolidate expenses by rpt and currency" do
+	it "should consolidate expenses by rpt and currency considering conversion rate" do
 		persisted_expenses  = [
-								Expense.new(expense_rpt_id: '123',original_currency: 'EUR', original_cost:'200'),
-								Expense.new(expense_rpt_id: '122',original_currency: 'EUR', original_cost:'100'),
-								Expense.new(expense_rpt_id: '121',original_currency: 'INR', original_cost:'1000'),
-								Expense.new(expense_rpt_id: '121',original_currency: 'EUR', original_cost:'100'),
-								Expense.new(expense_rpt_id: '121',original_currency: 'EUR', original_cost:'200')
+								Expense.new(expense_rpt_id: '123',original_currency: 'EUR', original_cost:'200', cost_in_home_currency:'200'),
+								Expense.new(expense_rpt_id: '122',original_currency: 'EUR', original_cost:'100', cost_in_home_currency:'100'),
+								Expense.new(expense_rpt_id: '121',original_currency: 'INR', original_cost:'1000', cost_in_home_currency:'4000'),
+								Expense.new(expense_rpt_id: '121',original_currency: 'EUR', original_cost:'100', cost_in_home_currency:'100'),
+								Expense.new(expense_rpt_id: '121',original_currency: 'EUR', original_cost:'200', cost_in_home_currency:'200')
 							 ]
 		forex_payments = [ForexPayment.new(:currency => 'EUR', :inr => 100, :amount => 100)] 
 		Expense.stub(:find).and_return(persisted_expenses)
@@ -17,7 +17,7 @@ describe 'expense_report' do
 		expected_hash_1 = {'report_id' => '123', 'currency' => 'EUR', 'amount' => 200, 'conversion_rate' => 1, 'local_currency_amount' => 200} 
 		expected_hash_2 = {'report_id' => '122', 'currency' => 'EUR', 'amount' => 100, 'conversion_rate' => 1, 'local_currency_amount' => 100} 
 		expected_hash_3 = {'report_id' => '121', 'currency' => 'EUR', 'amount' => 300, 'conversion_rate' => 1, 'local_currency_amount' => 300} 
-		expected_hash_4 = {'report_id' => '121', 'currency' => 'INR', 'amount' => 1000, 'conversion_rate' => 1, 'local_currency_amount' => 1000} 
+		expected_hash_4 = {'report_id' => '121', 'currency' => 'INR', 'amount' => 1000, 'conversion_rate' => 4, 'local_currency_amount' => 4000}
 		actual_consolidated = exp_rpt.consolidated_expenses
 		puts actual_consolidated.to_s
 		actual_consolidated.count.should ==4
