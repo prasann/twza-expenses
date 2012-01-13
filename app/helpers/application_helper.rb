1a8b219847df4ca84218cfd135b07f6a6913496f
TV Padmanabhan <padmana@thoughtworks.com>
2012-01-13 15:42:04 +0530

    Padmanabhan - Modified expense settlement to send notification and return to expense settlement tab with success flash message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,6 +6,19 @@ module ApplicationHelper
   end
 
   def format_two_decimal_places(number)
-	(number*100).round.to_f/100
+    (number*100).round.to_f/100
+  end
+
+  def flash_message
+    messages = ""
+
+    [:notice, :info, :warning, :error, :success].each { |type|
+      if flash[type]
+        div_tag = content_tag :div, flash[type], :class => "#{type} notification"
+        messages += div_tag
+      end
+    }
+    messages << javascript_include_tag("$('.notification').fadeOut(5000);")
+    messages.html_safe
   end
 end
