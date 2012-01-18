49c0611d78e7791cf5cf59817c184a83ebe598e5
Prasanna <pvenkat4ever@gmail.com>
2012-01-18 18:44:33 +0530

    Prasanna: Added forex reports mandatory fields

--- app/controllers/forex_reports_controller.rb
+++ app/controllers/forex_reports_controller.rb
@@ -3,7 +3,7 @@ require "#{Rails.root}/lib/helpers/excel_data_exporter"
 
 class ForexReportsController < ApplicationController
   include ExcelDataExporter
-  
+
   HEADERS = [
     'Month','EMP ID','Employee\'s Name','Forex Amt','Fx Crrn','Travelled Date','Place','Project',
     'Vendor Name','Card No','Exp Date','Office','INR'
@@ -20,8 +20,12 @@ class ForexReportsController < ApplicationController
 
   private
   def get_results
-  	return ForexPayment.where(currency: params[:forex_payment_currency])
+    return ForexPayment.where(:currency => regex_if_nil(params[:forex_payment_currency]))
     .and("issue_date" => {"$gte" => params[:reports_from], "$lte" => params[:reports_till]})
-    .and(vendor_name:params[:forex_payment_vendor_name])
+    .and(vendor_name: regex_if_nil(params[:forex_payment_vendor_name]))
+  end
+
+  def regex_if_nil(val)
+    return val.blank? ? /(\S)*/ : val
   end
 end
