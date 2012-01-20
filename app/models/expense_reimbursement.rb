883c82e3e56307c47e1ae0f17ac8087e21634f92
Praveena G <praveena.gunasekhar@gmail.com>
2012-01-20 18:41:24 +0530

    Praveena : Displayed modified amount in show

--- app/models/expense_reimbursement.rb
+++ app/models/expense_reimbursement.rb
@@ -1,12 +1,12 @@
 class ExpenseReimbursement
-	include Mongoid::Document
-	include Mongoid::Timestamps
+  include Mongoid::Document
+  include Mongoid::Timestamps
 
-	field :expense_report_id, type: Integer
-	field :empl_id, type: String
-	field :expenses, type: Array
-	field :status, type: String
-	field :submitted_on, type: Date
-	field :total_amount, type: Float
-	field :notes, type: String
+  field :expense_report_id, type: Integer
+  field :empl_id, type: String
+  field :expenses, type: Array
+  field :status, type: String
+  field :submitted_on, type: Date
+  field :total_amount, type: Float
+  field :notes, type: String
 end
