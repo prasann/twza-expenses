0385091ad70c06df6f297004aa4324eed5cee010
Praveena G <praveena.gunasekhar@gmail.com>
2012-01-24 12:23:16 +0530

    Praveena/Rajiv : modified original_cost to use cost in home currency instead

--- app/controllers/expense_reimbursement_controller.rb
+++ app/controllers/expense_reimbursement_controller.rb
@@ -4,43 +4,42 @@ class ExpenseReimbursementController < ApplicationController
     render :layout => 'tabs'
   end
 
+  def collect_reimbursement_expense_ids
+    reimbursement_expense_ids=[]
+    @expense_reimbursements.each do |expense_reimbursement|
+      reimbursement_expense_ids.push(expense_reimbursement.expenses.collect { |expense| expense['expense_id'] })
+    end
+    reimbursement_expense_ids
+  end
+
+  def create_unprocessed_expense_reports(empl_id, unprocessed_expenses)
+    unprocessed_expenses.each do |expense_report_id, expenses|
+      if (!expenses.empty?)
+        @expense_reimbursements.push(ExpenseReimbursement.new(:expense_report_id => expense_report_id,
+                                                              :empl_id => empl_id,
+                                                              :status => 'Unprocessed',
+                                                              :submitted_on=> expenses.first.report_submitted_at,
+                                                              :total_amount => expenses.sum { |expense| expense.cost_in_home_currency.to_f }))
+      end
+    end
+  end
+
   def filter
     @expense_reimbursements=[]
     if (!params[:empl_id].blank?)
-      @expense_reimbursements=ExpenseReimbursement.where(empl_id: params[:empl_id]).to_a
-      expense_ids_from_travel = ExpenseSettlement.where(empl_id: params[:empl_id]).to_a.collect { |settlement| settlement.expenses }.flatten
-      reimbursement_expense_ids=[]
-      @expense_reimbursements.each do |expense_reimbursement|
-        reimbursement_expense_ids.push(expense_reimbursement.expenses.collect { |expense| expense['expense_id'] })
-      end
+      empl_id=params[:empl_id]
+      @expense_reimbursements = ExpenseReimbursement.where(empl_id: empl_id).to_a
+      expense_ids_from_travel = ExpenseSettlement.where(empl_id: empl_id).to_a.collect { |settlement| settlement.expenses }.flatten
+      reimbursement_expense_ids = collect_reimbursement_expense_ids()
       processed_expense_ids = reimbursement_expense_ids.flatten.push(expense_ids_from_travel).flatten
-      unprocessed_expenses = Expense.fetch_for_employee(params[:empl_id], processed_expense_ids).group_by(&:expense_rpt_id)
-      unprocessed_expenses.each do |expense_report_id, expenses|
-        if (!expenses.empty?)
-          @expense_reimbursements.push(ExpenseReimbursement.new(:expense_report_id => expense_report_id,
-                                                                :empl_id => params[:empl_id],
-                                                                :status => 'Unprocessed',
-                                                                :submitted_on=> expenses.first.report_submitted_at,
-                                                                :total_amount => expenses.sum { |expense| expense.original_cost.to_f }))
-        end
-      end
+      unprocessed_expenses = Expense.fetch_for_employee(empl_id, processed_expense_ids).group_by(&:expense_rpt_id)
+      create_unprocessed_expense_reports(empl_id, unprocessed_expenses)
     elsif (!params[:expense_rpt_id].blank?)
       @expense_reimbursements=ExpenseReimbursement.where(expense_report_id: params[:expense_rpt_id]).to_a
-      reimbursement_expense_ids=[]
-      @expense_reimbursements.each do |expense_reimbursement|
-        reimbursement_expense_ids.push(expense_reimbursement.expenses.collect { |expense| expense['expense_id'] })
-      end
+      reimbursement_expense_ids = collect_reimbursement_expense_ids()
       unprocessed_expenses = Expense.fetch_for_report(params[:expense_rpt_id], reimbursement_expense_ids.flatten).group_by(&:expense_rpt_id)
-      unprocessed_expenses.each do |expense_report_id, expenses|
-        if (!expenses.empty?)
-          @expense_reimbursements.push(ExpenseReimbursement.new(:expense_report_id => expense_report_id,
-                                                                :empl_id => expenses.first.get_employee_id,
-                                                                :status => 'Unprocessed',
-                                                                :submitted_on=> expenses.first.report_submitted_at,
-                                                                :total_amount => expenses.sum { |expense| expense.original_cost.to_f }))
-        end
-      end
-
+      empl_id = unprocessed_expenses[params[:expense_rpt_id]].get_employee_id
+      create_unprocessed_expense_reports(empl_id, unprocessed_expenses)
     end
     render 'index', :layout => 'tabs'
   end
@@ -57,7 +56,7 @@ class ExpenseReimbursementController < ApplicationController
     @expense_reimbursement = {'expense_report_id' => params[:id],
                               'empl_id' => expenses.first.get_employee_id,
                               'submitted_on' => expenses.first.report_submitted_at,
-                              'total_amount' => expenses.sum { |expense| expense.original_cost.to_f }}
+                              'total_amount' => expenses.sum { |expense| expense.cost_in_home_currency.to_f }}
 
   end
 
@@ -67,8 +66,10 @@ class ExpenseReimbursementController < ApplicationController
     total_amount = 0
     expenses = []
 
+    expense_map = Hash.new()
+    Expense.fetch_for_report(params[:expense_report_id],[]).map{|expense| expense_map[expense.id.to_s] = expense}
     expense_ids.each do |expense_id|
-      modified_amount=expense_amount[expense_id].to_f
+      modified_amount=expense_amount[expense_id].to_f*expense_map[expense_id].cost_in_home_currency.to_f/expense_map[expense_id].original_cost.to_f
       expenses.push({'expense_id' => expense_id, 'modified_amount' => modified_amount})
       total_amount+=modified_amount
     end
