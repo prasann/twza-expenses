889be5a296e2f579b9829d9ceeaac4dc4fc14e87
Praveena G <praveena.gunasekhar@gmail.com>
2012-01-25 15:58:20 +0530

    Praveena : Made total to reflect on change of checkbox selection and changing values in text box

---
+++ app/assets/javascripts/expense_reimbursement.js
@@ -0,0 +1,13 @@
+function calculateTotal() {
+    var total_amount = 0;
+    $(':checked').each(function() {
+        var expense_id = $(this).val();
+        var amount = $("[name='expense_amount[" + expense_id + "]']").val();
+        total_amount= parseFloat(total_amount)+parseFloat(amount);
+    });
+    $("#total_amount").text(total_amount);
+};
+
+$(document).ready(function() {
+    $(".affects_total").change(calculateTotal);
+});
