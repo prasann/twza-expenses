980d59423ecf5a28c0a82d3a371a3f3aa361eca6
Prasanna <pvenkat4ever@gmail.com>
2012-01-27 11:20:04 +0530

    Prasanna: Added jeditable and datepicker to it

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,30 +9,52 @@
 //= require jquery-ui
 //= require_tree .
 $(document).ready(function(){
-	$(function($) {
-   		$('#menu').tabs({
-			ajaxOptions: {
-				error: function( xhr, status, index, anchor ) {
-					$( anchor.hash ).html(
-						"Couldn't load this tab. We'll try to fix this as soon as possible. " +
-						"If this wouldn't be a demo." );
-				}
-			},
-			select: function(event, ui){
-   				 if(ui.index == 0){ window.location = "/outbound_travels#outbound_travels";}
-   				 if(ui.index == 1){ window.location = "/forex_payments#forex_payments";} 
-   				 if(ui.index == 2){ window.location = "/expense_settlement#expense_settlement";} 
-   				 if(ui.index == 3){ window.location = "/expense_reimbursement#expense_reimbursement";} 
-  			}
-		});
+  $(function($) {
+    $('#menu').tabs({
+      ajaxOptions: {
+        error: function( xhr, status, index, anchor ) {
+          $( anchor.hash ).html(
+            "Couldn't load this tab. We'll try to fix this as soon as possible. " +
+            "If this wouldn't be a demo." );
+        }
+      },
+      select: function(event, ui){
+        if(ui.index == 0){ window.location = "/outbound_travels#outbound_travels";}
+        if(ui.index == 1){ window.location = "/forex_payments#forex_payments";} 
+        if(ui.index == 2){ window.location = "/expense_settlement#expense_settlement";} 
+        if(ui.index == 3){ window.location = "/expense_reimbursement#expense_reimbursement";} 
+      }
+    });
 
-		$('.edit').editable('http://www.example.com/save.php', { 
-	         id   : 'elementid',
-	         name : 'newvalue'
-     	});
+    $('.edit_date_picker').editable(function(value, settings) {   	
+      saveValue(value,settings,this)
+    }, {
+      type    : 'datepicker'
+    });
 
-		$('.date_picker').datepicker({
-			dateFormat: 'dd-M-yy'
-		});
-	});  
+    $('.edit').editable(function(value, settings) {   	
+      saveValue(value,settings,this)
+    }, {
+      type    : 'text',
+      submit : 'Save',
+      cancel : 'Cancel'
+    });
+
+    $('.date_picker').datepicker(
+    	 "option", "dateFormat", "dd-mm-yy"
+    );
+
+    function saveValue(value,settings,_this){
+      var obj_id = $(_this).parent().attr('id');
+      var attr_name = $(_this).attr('name');  
+      $.ajax({
+        type:'POST',
+        url:'/outbound_travels/update_field',
+        data:{id: obj_id, name: attr_name, value: value},
+        success: function(){
+          $(_this).text(value)		
+        }
+      })
+    }
+  });  
 });
