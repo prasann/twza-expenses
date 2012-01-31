fd5573d48f7292f40622958097b08e5b71dd652a
Praveena G <praveena.gunasekhar@gmail.com>
2012-01-31 19:26:52 +0530

    Praveena : Added test for the expense reimbursement controller

--- app/controllers/expense_reimbursement_controller.rb
+++ app/controllers/expense_reimbursement_controller.rb
@@ -4,6 +4,84 @@ class ExpenseReimbursementController < ApplicationController
     render :layout => 'tabs'
   end
 
+  def filter
+    @expense_reimbursements=[]
+    if (!params[:empl_id].blank?)
+      empl_id=params[:empl_id]
+      @expense_reimbursements = ExpenseReimbursement.where(empl_id: empl_id).to_a
+      expenses_criteria = Expense.create_employee_id_criteria(empl_id)
+    elsif (!params[:expense_rpt_id].blank?)
+      @expense_reimbursements=ExpenseReimbursement.where(expense_report_id: params[:expense_rpt_id]).to_a
+      expenses_criteria = Expense.where(expense_rpt_id: params[:expense_rpt_id])
+      expense = expenses_criteria.first
+      empl_id = expense.nil? ? "" : expense.get_employee_id
+    end
+
+    if (empl_id)
+      expense_ids_from_travel = ExpenseSettlement.where(empl_id: empl_id).to_a.collect { |settlement| settlement.expenses }.flatten
+      reimbursement_expense_ids = collect_reimbursement_expense_ids()
+      processed_expense_ids = reimbursement_expense_ids.flatten.push(expense_ids_from_travel).flatten
+
+      unprocessed_expenses_map = Expense.fetch_for(expenses_criteria, processed_expense_ids).group_by(&:expense_rpt_id)
+      create_unprocessed_expense_reports(empl_id, unprocessed_expenses_map)
+    end
+
+    render 'index', :layout => 'tabs'
+  end
+
+  def show
+    @expense_reimbursement=ExpenseReimbursement.find(params[:id])
+    profile = Profile.find_by_employee_id(@expense_reimbursement.empl_id)
+    @empl_name = profile.nil? ? "" : profile.get_full_name
+    @all_expenses = @expense_reimbursement.get_expenses_grouped_by_project_code
+  end
+
+  def edit
+    expenses = Expense.where(expense_rpt_id: params[:id]).to_a
+
+    employee = Profile.find_by_employee_id(expenses.first.get_employee_id.to_i)
+    @empl_name = employee.nil? ? "" : employee.get_full_name
+
+    existing_expense_reimbursements = ExpenseReimbursement.where(expense_report_id: params[:id]).to_a
+
+    if (existing_expense_reimbursements.empty?)
+      @all_expenses = expenses.group_by { |expense| expense.project + expense.subproject }
+    else
+      expenses = expenses-existing_expense_reimbursements.collect { |existing_expense_reimbursement| existing_expense_reimbursement.get_expenses }.flatten
+      @all_expenses = expenses.group_by { |expense| expense.project + expense.subproject }
+    end
+
+    @expense_reimbursement = {'expense_report_id' => params[:id],
+                              'empl_id' => expenses.first.get_employee_id,
+                              'submitted_on' => expenses.first.report_submitted_at,
+                              'total_amount' => expenses.sum { |expense| expense.cost_in_home_currency.to_f }}
+
+  end
+
+  def process_reimbursement
+    expense_ids = params[:selected_expenses]
+    expense_amount = params[:expense_amount]
+    total_amount = 0
+    expenses = []
+
+    expense_map = Hash.new()
+    Expense.where(expense_rpt_id: params[:expense_report_id]).to_a.map { |expense| expense_map[expense.id.to_s] = expense }
+    expense_ids.each do |expense_id|
+      modified_amount=expense_amount[expense_id].to_f
+      expenses.push({'expense_id' => expense_id, 'modified_amount' => modified_amount})
+      total_amount+=modified_amount
+    end
+    status = params[:process_reimbursement] ? 'Processed' : 'Faulty'
+    @expense_reimbursement = ExpenseReimbursement.create(:expense_report_id => params[:expense_report_id], :expenses => expenses,
+                                                         :empl_id => params[:empl_id], :status=> status,
+                                                         :submitted_on=> params[:submitted_on], :notes=> params[:notes],
+                                                         :total_amount => total_amount)
+    profile = Profile.find_by_employee_id(@expense_reimbursement.empl_id)
+    EmployeeMailer.non_travel_expense_reimbursement(profile, @expense_reimbursement).deliver
+    redirect_to :action => 'filter', :empl_id => params[:empl_id]
+  end
+
+  private
   def collect_reimbursement_expense_ids
     reimbursement_expense_ids=[]
     @expense_reimbursements.each do |expense_reimbursement|
@@ -23,84 +101,5 @@ class ExpenseReimbursementController < ApplicationController
       end
     end
   end
-
-  def filter
-    @expense_reimbursements=[]
-    
-	if (!params[:empl_id].blank?)
-      empl_id=params[:empl_id]
-      @expense_reimbursements = ExpenseReimbursement.where(empl_id: empl_id).to_a
-      expenses_criteria = Expense.create_employee_id_criteria(empl_id)
-    elsif (!params[:expense_rpt_id].blank?)
-      @expense_reimbursements=ExpenseReimbursement.where(expense_report_id: params[:expense_rpt_id]).to_a
-      expenses_criteria = Expense.where(expense_rpt_id: params[:expense_rpt_id])
-      expense = expenses_criteria.first
-	  empl_id = expense.nil? ? "" : expense.get_employee_id
-    end
-	
-	if(empl_id)
-		expense_ids_from_travel = ExpenseSettlement.where(empl_id: empl_id).to_a.collect { |settlement| settlement.expenses }.flatten
-		reimbursement_expense_ids = collect_reimbursement_expense_ids()
-		processed_expense_ids = reimbursement_expense_ids.flatten.push(expense_ids_from_travel).flatten
-		
-		unprocessed_expenses_map = Expense.fetch_for(expenses_criteria, processed_expense_ids).group_by(&:expense_rpt_id)
-		create_unprocessed_expense_reports(empl_id, unprocessed_expenses_map)
-    end
-
-	render 'index', :layout => 'tabs'
-  end
-
-  def show
-    @expense_reimbursement=ExpenseReimbursement.find(params[:id])
-	profile = Profile.find_by_employee_id(@expense_reimbursement.empl_id) 
-    @empl_name = profile.nil? ? "" : profile.get_full_name
-    @all_expenses = @expense_reimbursement.get_expenses_grouped_by_project_code
-  end
-
-  def edit
-    expenses = Expense.where(expense_rpt_id: params[:id]).to_a
-    
-	employee = Profile.find_by_employee_id(expenses.first.get_employee_id.to_i)
-	@empl_name = employee.nil? ? "" : employee.get_full_name
-    
-	existing_expense_reimbursements = ExpenseReimbursement.where(expense_report_id: params[:id]).to_a
-    
-	if (existing_expense_reimbursements.empty?)
-      @all_expenses = expenses.group_by { |expense| expense.project + expense.subproject }
-    else
-      expenses = expenses-existing_expense_reimbursements.collect{ |existing_expense_reimbursement| existing_expense_reimbursement.get_expenses }.flatten
-      @all_expenses = expenses.group_by { |expense| expense.project + expense.subproject }
-    end
-    
-	@expense_reimbursement = {'expense_report_id' => params[:id],
-                              'empl_id' => expenses.first.get_employee_id,
-                              'submitted_on' => expenses.first.report_submitted_at,
-                              'total_amount' => expenses.sum { |expense| expense.cost_in_home_currency.to_f }}
-
-  end
-
-  def process_reimbursement
-    expense_ids = params[:selected_expenses]
-    expense_amount = params[:expense_amount]
-    total_amount = 0
-    expenses = []
-
-    expense_map = Hash.new()
-    Expense.where(expense_rpt_id: params[:expense_report_id]).to_a.map { |expense| expense_map[expense.id.to_s] = expense }
-    expense_ids.each do |expense_id|
-      modified_amount=expense_amount[expense_id].to_f       
-	  expenses.push({'expense_id' => expense_id, 'modified_amount' => modified_amount})
-      total_amount+=modified_amount
-    end
-    status = params[:process_reimbursement] ? 'Processed' : 'Faulty'
-    @expense_reimbursement = ExpenseReimbursement.create(:expense_report_id => params[:expense_report_id], :expenses => expenses,
-                                                      :empl_id => params[:empl_id], :status=> status,
-                                                      :submitted_on=> params[:submitted_on], :notes=> params[:notes],
-                                                      :total_amount => total_amount)
-    profile = Profile.find_by_employee_id(@expense_reimbursement.empl_id)
-    EmployeeMailer.non_travel_expense_reimbursement(profile, @expense_reimbursement).deliver
-    redirect_to :action => 'filter', :empl_id => params[:empl_id]
-  end
-
 
 end
---
+++ spec/controllers/expense_reimbursement_controller_spec.rb
@@ -0,0 +1,86 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe ExpenseReimbursementController do
+
+  describe "Get show " do
+
+    it "should show expense reimbursement details" do
+      expected_expense_reimbursement = mock("expense_reimbursement", :id => 123, :empl_id => 1234)
+      profile = mock('profile')
+      expected_expenses = mock('expense')
+      #expense_reimbursement_2 = mock("expense_reimbursement", :id => 231, :empl_id => 231)
+
+      ExpenseReimbursement.should_receive(:find).with("123").and_return(expected_expense_reimbursement)
+      #Expense.should_receive(:create_employee_id_criteria).with("123").and_return(criteria)
+
+      Profile.should_receive(:find_by_employee_id).with(1234).and_return(profile)
+      profile.should_receive(:get_full_name).and_return('John Smith')
+      expected_expense_reimbursement.should_receive(:get_expenses_grouped_by_project_code).and_return(expected_expenses)
+
+      get :show, :id => 123
+      assigns(:expense_reimbursement).should == expected_expense_reimbursement
+      assigns(:all_expenses).should == expected_expenses
+      assigns(:empl_name).should == 'John Smith'
+    end
+  end
+
+  describe "Get edit " do
+
+    it "should load only expenses which are not processed as part of another expense reimbursement" do
+      expense_1 = mock('expense', :project=>'project', :subproject => 'subproject',
+                       :cost_in_home_currency=>1000, :get_employee_id=>1234, :report_submitted_at => 'date')
+      expense_2 = mock('expense', :project=>'project', :subproject => 'subproject',
+                       :cost_in_home_currency=>200, :get_employee_id=>1234, :report_submitted_at => 'date')
+
+      existing_expense_reimbursement = mock("expense_reimbursement", :id => 123,
+                                            :empl_id => 1234, :expense_report_id => 12345, :get_expenses=>[expense_2])
+      profile = mock('profile')
+      expected_expenses = {"projectsubproject"=>[expense_1]}
+
+      expected_expense_reimbursement = {'expense_report_id' => '123',
+                                        'empl_id' => 1234,
+                                        'submitted_on' => 'date',
+                                        'total_amount' => 1000.0}
+
+      ExpenseReimbursement.stub_chain(:where, :to_a).and_return([existing_expense_reimbursement])
+      Expense.stub_chain(:where, :to_a).and_return([expense_1, expense_2])
+
+      Profile.should_receive(:find_by_employee_id).with(1234).and_return(profile)
+      profile.should_receive(:get_full_name).and_return('John Smith')
+
+      get :edit, :id => 123
+      assigns(:expense_reimbursement).should == expected_expense_reimbursement
+      assigns(:all_expenses).count == 1
+      assigns(:all_expenses).should == expected_expenses
+      assigns(:empl_name).should == 'John Smith'
+    end
+
+    it "should load all expenses for new expense reimbursement" do
+      expense_1 = mock('expense', :project=>'project', :subproject => 'subproject',
+                       :cost_in_home_currency=>1000, :get_employee_id=>1234, :report_submitted_at => 'date')
+      expense_2 = mock('expense', :project=>'project', :subproject => 'subproject',
+                       :cost_in_home_currency=>200, :get_employee_id=>1234, :report_submitted_at => 'date')
+
+      profile = mock('profile')
+      expected_expenses = {"projectsubproject"=>[expense_1, expense_2]}
+
+      expected_expense_reimbursement = {'expense_report_id' => '123',
+                                        'empl_id' => 1234,
+                                        'submitted_on' => 'date',
+                                        'total_amount' => 1200.0}
+
+      ExpenseReimbursement.stub_chain(:where, :to_a).and_return([])
+      Expense.stub_chain(:where, :to_a).and_return([expense_1, expense_2])
+
+      Profile.should_receive(:find_by_employee_id).with(1234).and_return(profile)
+      profile.should_receive(:get_full_name).and_return('John Smith')
+
+      get :edit, :id => 123
+      assigns(:expense_reimbursement).should == expected_expense_reimbursement
+      assigns(:all_expenses).count == 1
+      assigns(:all_expenses).should == expected_expenses
+      assigns(:empl_name).should == 'John Smith'
+    end
+  end
+
+end
