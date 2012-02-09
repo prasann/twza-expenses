459c4b5002b157de341a0a56bf90cd924010b5bc
Vijay Aravamudhan <avijayr - at - gmail - dot - com>
2012-02-09 14:17:50 +0530

    Vijay: For non-prod environments, the email that gets generated needs to be distinguished from the prod emails - so prepend the rails environment for this distinction.

--- app/mailers/employee_mailer.rb
+++ app/mailers/employee_mailer.rb
@@ -13,6 +13,7 @@ class EmployeeMailer < ActionMailer::Base
     @profile = @expense_report.profile
     travel = @expense_report.outbound_travel
     subject = EXPENSE_SETTLEMENT_SUBJECT.sub("$place", travel.place).sub('$start_date', DateHelper.date_fmt(travel.departure_date))
+    subject.insert(0, "#{Rails.env} - ") unless Rails.env.production?
 
     mail(:to => @expense_report.employee_email, :subject => subject, :content_type => "text/html") do |format|
       format.html { render :action => 'expense_settlement' }
@@ -26,6 +27,7 @@ class EmployeeMailer < ActionMailer::Base
     @empl_name = @profile.get_full_name
 
     subject = EXPENSE_REIMBURSEMENT_SUBJECT.sub('$expense_report_id', @expense_reimbursement.expense_report_id.to_s)
+    subject.insert(0, "#{Rails.env} - ") unless Rails.env.production?
 
     mail(:to => @expense_reimbursement.employee_email, :subject => subject, :content_type => "text/html") do |format|
       format.html { render :action => 'non_travel_expense_reimbursement' }
