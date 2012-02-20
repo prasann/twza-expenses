10bde4964983e329caa74de9fd5ddeea9f5d4be8
Vijay Aravamudhan <avijayr - at - gmail - dot - com>
2012-02-21 03:34:58 +0530

    Vijay: Removed unused homes controller and its view.

--- app/controllers/homes_controller.rb
+++
@@ -1,6 +0,0 @@
-# TODO: This controller should not even be needed - use routes to limit?
-class HomeController < ApplicationController
-  def index
-    redirect_to :controller => 'outbound_travels', :action => 'index'
-  end
-end
