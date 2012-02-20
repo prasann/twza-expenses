cc8b71021994295e689966565e55989aacc81070
Vijay Aravamudhan <avijayr - at - gmail - dot - com>
2012-02-21 02:44:28 +0530

    Vijay: Fixing broken excel export.

--- lib/helpers/excel_data_exporter.rb
+++ lib/helpers/excel_data_exporter.rb
@@ -1,12 +1,13 @@
 module ExcelDataExporter
   FILE_EXTENSION = ".xls"
+  MASKED_FIELD_NAMES = ['_id', '_type', 'nil']
 
   def export_xls(data_to_export, headers, options)
     respond_to do |format|
       format.xls do
         model = options[:model]
         filename_prefix = options[:filename_prefix] || model
-        exportable_fields = options[:exportable_fields] || model.fields.select_map { |field| field[1].name if !['_id', '_type', 'nil'].include?(field[1].name) }
+        exportable_fields = options[:exportable_fields] || model.fields.keys.select { |name| name if !MASKED_FIELD_NAMES.include?(name) }
         send_data(data_to_export.to_xls(:headers => headers, :columns => exportable_fields),
                   :filename => "#{filename_prefix}_#{DateHelper.date_fmt(Date.today)}#{FILE_EXTENSION}")
       end
