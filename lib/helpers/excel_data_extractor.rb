1326383318903fc83542d266172dd8856cf1a9d6
Vijay Aravamudhan <avijayr - at - gmail - dot - com>
2012-02-21 22:17:46 +0530

    Vijay: Fixing a bug in the extractor code; Adding a new TODO for Ajit's bug.

--- lib/helpers/excel_data_extractor.rb
+++ lib/helpers/excel_data_extractor.rb
@@ -5,48 +5,26 @@ module ExcelDataExtractor
   EXCEL_HANDLERS = {"xls" => Excel, "xlsx" => Excelx}
 
   def read_from_excel(file_name, sheet_id, &callback)
-    @ignored_records_count = 0
-    @exception_records_count = 0
+    ignored_records_count = 0
     file = handler(file_name)
     file.default_sheet = file.sheets[sheet_id]
     2.upto(file.last_row) do |line|
       extractor = Proc.new{ |column| file.cell(line, column) }
       record = callback.call(extractor)
-      begin
-        record.save!
-        # TODO: If you call save!, then this line will not be hit if there really are errors!
-        @ignored_records_count += handle_validation_error(record)
-      rescue => e
-        @exception_records_count += 1
-        $stderr.puts e
-        $stderr.puts "Error while processing the record: #{record.inspect}"
+      unless record.save
+        $stderr.puts "Record: #{record.inspect}, Errors: #{record.errors.full_messages.join(',')}"
+        ignored_records_count += 1
       end
     end
-    show_ignored_records_summary(@ignored_records_count)
-    show_exception_records_summary(@exception_records_count)
+    $stderr.puts "\n"
+    show_summary(ignored_records_count, "(out of a total count of #{file.last_row - 1}) ignored due to the above errors")
   end
 
   private
-  def handle_validation_error(record)
-    unless record.errors.empty?
-      $stderr.puts "\nRecord: #{record.inspect}, Error: #{record.errors.messages}"
-      return 1
-    end
-    return 0
-  end
-
-  def show_ignored_records_summary(record_count)
-    show_summary record_count, ' ignored due to validation errors'
-  end
-
-  def show_exception_records_summary(record_count)
-    show_summary record_count, ' could not be inserted due to database errors'
-  end
-
   def show_summary(records_count, summary_msg)
     if records_count > 0
       records_count_msg = ImportHelper::pluralize(records_count, 'record')
-      $stderr.puts "\n#{records_count_msg} #{summary_msg}"
+      $stderr.puts "#{records_count_msg} #{summary_msg}"
     end
   end
 
