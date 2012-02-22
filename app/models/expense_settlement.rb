50d3e94291e03a19ab3561e6b93601dbdee14271
Vijay Raghavan Aravamudhan <vraravam@thoughtworks.com>
2012-02-22 18:33:49 +0530

    Vijay: Fixing issue where the cash handover amount could be nil.

--- app/models/expense_settlement.rb
+++ app/models/expense_settlement.rb
@@ -151,7 +151,9 @@ class ExpenseSettlement
     @cash_handover_total = 0
     @conversion_rates ||= get_conversion_rates_for_currency
     self.cash_handovers.each do |cash_handover|
-      @cash_handover_total += cash_handover.amount * @conversion_rates[cash_handover.currency]
+      if cash_handover.amount.present? && cash_handover.currency.present?
+        @cash_handover_total += cash_handover.amount * @conversion_rates[cash_handover.currency]
+      end
     end
     value = forex_inr_amount - (expense_inr_amount + @cash_handover_total)
     # TODO: Should not need to round off to 2 decimal places here - only in views
