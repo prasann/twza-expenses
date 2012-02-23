a2a1f7fd78d9a24bc4facb2f05b6ad62e5caa411
Vijay Raghavan Aravamudhan <vraravam@thoughtworks.com>
2012-02-24 02:00:05 +0530

    Vijay: Moved the tabs js code from the generic application.js file into the tabs erb file where this is actually used.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,24 +9,8 @@
 //= require jquery-ui
 //= require_tree .
 
+// TODO: The above require tree is bad - it will send the all js files for all views
 // TODO: Move this to the specific pages that need these functions defined
 $(document).ready(function() {
-  $('#menu').tabs({
-    ajaxOptions: {
-      error: function(xhr, status, index, anchor) {
-        $(anchor.hash).html("Couldn't load this tab. We'll try to fix this as soon as possible. If this wouldn't be a demo.");
-      }
-    },
-    select: function(event, ui) {
-      // TODO: This is somehow broken by Vijay - Need help to investigate and fix
-      // TODO: Use url helpers rather than hardcoding the url
-      if (ui.index == 0) { window.location = "/outbound_travels#outbound_travels";}
-      if (ui.index == 1) { window.location = "/forex_payments#forex_payments";}
-      if (ui.index == 2) { window.location = "/expense_settlements#expense_settlements";}
-      if (ui.index == 3) { window.location = "/expense_reimbursements#expense_reimbursements";}
-      if (ui.index == 4) { window.location = "/reports#reports";}
-    }
-  });
-
   $('.date_picker').datepicker({dateFormat : 'dd-M-yy'});
 });
