df1d40655a49779ab6c1312c2385121c6f8b7e9a
Prasanna <prasann@thoughtworks.com>
2012-02-27 17:49:15 +0530

    Prasanna: Fixing the broken specs

--- spec/controllers/forex_reports_controller_spec.rb
+++ spec/controllers/forex_reports_controller_spec.rb
@@ -7,13 +7,13 @@ describe ForexReportsController do
       forex_payments_2 = Factory(:forex_payment, :vendor_name => 'B', :currency => 'USD', :issue_date => Date.parse('31-12-2011'))
       forex_payments_3 = Factory(:forex_payment, :vendor_name => 'B', :currency => 'EUR', :issue_date => Date.parse('31-12-2011'))
 
-      get :search, :forex_payment_currency => 'USD', :reports_from => Date.parse('1-12-2011'), :reports_till => Date.parse('31-12-2011'), :forex_payment_vendor_name => 'A'
+      get :index, :forex_payment_currency => 'USD', :reports_from => Date.parse('1-12-2011'), :reports_till => Date.parse('31-12-2011'), :forex_payment_vendor_name => 'A'
       assigns(:forex_payments).should eq([forex_payments_1])
 
-      get :search, :forex_payment_currency => 'USD', :reports_from => Date.parse('1-12-2011'), :reports_till => Date.parse('30-12-2011'), :forex_payment_vendor_name => 'A'
+      get :index, :forex_payment_currency => 'USD', :reports_from => Date.parse('1-12-2011'), :reports_till => Date.parse('30-12-2011'), :forex_payment_vendor_name => 'A'
       assigns(:forex_payments).should eq([])
 
-      get :search, :forex_payment_currency => 'EUR', :reports_from => Date.parse('1-12-2011'), :reports_till => Date.parse('31-12-2011'), :forex_payment_vendor_name => 'B'
+      get :index, :forex_payment_currency => 'EUR', :reports_from => Date.parse('1-12-2011'), :reports_till => Date.parse('31-12-2011'), :forex_payment_vendor_name => 'B'
       assigns(:forex_payments).should eq([forex_payments_3])
     end
   end
