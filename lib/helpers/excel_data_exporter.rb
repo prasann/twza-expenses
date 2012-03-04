4a96828be147a98463315321de5a54377d2bce07
Vijay Raghavan Aravamudhan <vraravam@thoughtworks.com>
2012-03-05 00:27:06 +0530

    Vijay: Dont need to only respond to xls format - if this method is called, then the response format should be xls.

--- lib/helpers/excel_data_exporter.rb
+++ lib/helpers/excel_data_exporter.rb
@@ -3,14 +3,10 @@ module ExcelDataExporter
   MASKED_FIELD_NAMES = ['_id', '_type', 'nil']
 
   def export_xls(data_to_export, headers, options)
-    respond_to do |format|
-      format.xls do
-        model = options[:model]
-        filename_prefix = options[:filename_prefix] || model
-        exportable_fields = options[:exportable_fields] || model.fields.keys.select { |name| name if !MASKED_FIELD_NAMES.include?(name) }
-        send_data(data_to_export.to_xls(:headers => headers, :columns => exportable_fields),
-                  :filename => "#{filename_prefix}_#{DateHelper.date_fmt(Date.today)}#{FILE_EXTENSION}")
-      end
-    end
+    model = options[:model]
+    filename_prefix = options[:filename_prefix] || model
+    exportable_fields = options[:exportable_fields] || model.fields.keys.select { |name| name if !MASKED_FIELD_NAMES.include?(name) }
+    send_data(data_to_export.to_xls(:headers => headers, :columns => exportable_fields),
+              :filename => "#{filename_prefix}_#{DateHelper.date_fmt(Date.today)}#{FILE_EXTENSION}")
   end
 end
