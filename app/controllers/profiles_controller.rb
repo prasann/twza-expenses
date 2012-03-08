39a073ea21b2b5e257c942a286a996406f723f89
Vijay Raghavan Aravamudhan <vraravam@thoughtworks.com>
2012-03-08 05:31:04 +0530

    Vijay:Adding TODOs.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,12 +2,14 @@ class ProfilesController < ApplicationController
   def search_by_name
     # TODO: sanitize param - SQL injection vulnerability
     @profiles = Profile.where("common_name like '#{params[:term]}%'")
+    # TODO: Why not pass pure json without special formatting - the current way, the text is split back into individual attributes on the js side
     render :text => @profiles.collect(&:to_special_s)
   end
 
   def search_by_id
     # TODO: sanitize param - SQL injection vulnerability
     @profiles = Profile.where("employee_id like '#{params[:term]}%'")
+    # TODO: Why not pass pure json without special formatting - the current way, the text is split back into individual attributes on the js side
     render :text => @profiles.collect(&:to_special_s)
   end
 end
