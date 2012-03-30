786903839d36bb53f88e519ba500424752e05d02
Vijay Raghavan Aravamudhan <vraravam@thoughtworks.com>
2012-03-30 14:35:56 +0530

    Vijay: Added ability to search by part of the employee name or id; Also fixed sql injection issue.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,13 +1,11 @@
 class ProfilesController < ApplicationController
   def search_by_name
-    # TODO: sanitize param - SQL injection vulnerability
-    @profiles = Profile.where("common_name like '#{params[:term]}%'")
+    @profiles = Profile.where("common_name like ?", "%#{params[:term]}%")
     render :json => @profiles.to_json(:root => false, :only => [:employee_id, :common_name])
   end
 
   def search_by_id
-    # TODO: sanitize param - SQL injection vulnerability
-    @profiles = Profile.where("employee_id like '#{params[:term]}%'")
+    @profiles = Profile.where("employee_id like ?", "%#{params[:term]}%")
     render :json => @profiles.to_json(:root => false, :only => [:employee_id, :common_name])
   end
 end
--- spec/controllers/profiles_controller_spec.rb
+++ spec/controllers/profiles_controller_spec.rb
@@ -1,30 +1,36 @@
 require 'spec_helper'
 
 describe ProfilesController do
+  before(:each) do
+    Profile.any_instance.stub(:readonly?).and_return(false)
+  end
+
   it "should fetch the profile for the given name using like" do
-    profile_1 = Profile.new(:common_name => "test", :employee_id => "1")
-    profile_2 = Profile.new(:common_name => "test a", :employee_id => "2")
-    Profile.should_receive(:where).with("common_name like 'test%'").and_return([profile_1, profile_2])
+    profile_1 = Profile.create!(:common_name => "test", :employee_id => "1", :name => "test", :email_id => "a@b.com")
+    profile_2 = Profile.create!(:common_name => "b test a", :employee_id => "2", :name => "test", :email_id => "a@b.com")
+    profile_3 = Profile.create!(:common_name => "b te st a", :employee_id => "3", :name => "test", :email_id => "a@b.com")
 
     get :search_by_name, :term => 'test'
 
     assigns(:profiles).size.should == 2
-    assigns(:profiles).first.should == profile_1
-    assigns(:profiles).last.should == profile_2
+    assigns(:profiles).collect(&:employee_id).should include("1")
+    assigns(:profiles).collect(&:employee_id).should include("2")
+    assigns(:profiles).collect(&:employee_id).should_not include("3")
     response.should be_success
-    response.body.should == "[{\"common_name\":\"test\",\"employee_id\":\"1\"},{\"common_name\":\"test a\",\"employee_id\":\"2\"}]"
+    response.body.should == "[{\"common_name\":\"test\",\"employee_id\":\"1\"},{\"common_name\":\"b test a\",\"employee_id\":\"2\"}]"
   end
 
   it "should fetch the profile for the given id using like" do
-    profile_1 = Profile.new(:common_name => "test", :employee_id => "1234")
-    profile_2 = Profile.new(:common_name => "test a", :employee_id => "123456")
-    Profile.should_receive(:where).with("employee_id like '1234%'").and_return([profile_1, profile_2])
+    profile_1 = Profile.create!(:common_name => "test", :employee_id => "1234", :name => "test", :email_id => "a@b.com")
+    profile_2 = Profile.create!(:common_name => "test a", :employee_id => "123456", :name => "test", :email_id => "a@b.com")
+    profile_3 = Profile.create!(:common_name => "a test b", :employee_id => "124356", :name => "test", :email_id => "a@b.com")
 
-    get :search_by_id, :term => '1234'
+    get :search_by_id, :term => '234'
 
     assigns(:profiles).size.should == 2
-    assigns(:profiles).first.should == profile_1
-    assigns(:profiles).last.should == profile_2
+    assigns(:profiles).collect(&:employee_id).should include("1234")
+    assigns(:profiles).collect(&:employee_id).should include("123456")
+    assigns(:profiles).collect(&:employee_id).should_not include("124356")
     response.should be_success
     response.body.should == "[{\"common_name\":\"test\",\"employee_id\":\"1234\"},{\"common_name\":\"test a\",\"employee_id\":\"123456\"}]"
   end
