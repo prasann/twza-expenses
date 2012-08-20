d51b13318e98b988c0e5706d788379dded7ce6df
Prasanna Venkatesan <pvenkat4ever@gmail.com>
2012-08-20 20:32:28 +0530

    Prasanna: Email Will be sent from expense info rather from employee info

--- app/mailers/employee_mailer.rb
+++ app/mailers/employee_mailer.rb
@@ -11,26 +11,30 @@ class EmployeeMailer < ActionMailer::Base
 
   def expense_settlement(expense_settlement)
     @expense_settlement = expense_settlement
-    @employee_detail = @expense_settlement.employee_detail
-    subject = EXPENSE_SETTLEMENT_SUBJECT.sub("$start_date", DateHelper.date_fmt(@expense_settlement.expense_from))
-                                        .sub('$end_date', DateHelper.date_fmt(@expense_settlement.expense_to))
+    @empl_name = @expense_settlement.employee_detail.try(:emp_name) || ""
+    subject = EXPENSE_SETTLEMENT_SUBJECT.sub("$start_date", DateHelper.date_fmt(@expense_settlement.expense_from)).sub('$end_date', DateHelper.date_fmt(@expense_settlement.expense_to))
     subject.insert(0, "#{Rails.env} - ") unless Rails.env.production?
 
-    mail(:to => @employee_detail.try(:email), :subject => subject, :content_type => "text/html") do |format|
+    mail(:to => to_address(expense_settlement.empl_id), :subject => subject, :content_type => "text/html") do |format|
       format.html { render :action => 'expense_settlement' }
     end
   end
 
   def non_travel_expense_reimbursement(expense_reimbursement)
     @expense_reimbursement = expense_reimbursement
+    @empl_name = @expense_reimbursement.employee_detail.try(:emp_name) || ""
     @employee_detail = @expense_reimbursement.employee_detail
     @all_expenses = @expense_reimbursement.get_expenses_grouped_by_project_code
 
     subject = EXPENSE_REIMBURSEMENT_SUBJECT.sub('$expense_report_id', @expense_reimbursement.expense_report_id.to_s)
     subject.insert(0, "#{Rails.env} - ") unless Rails.env.production?
 
-    mail(:to => @employee_detail.try(:email), :subject => subject, :content_type => "text/html") do |format|
+    mail(:to => to_address(expense_reimbursement.empl_id), :subject => subject, :content_type => "text/html") do |format|
       format.html { render :action => 'non_travel_expense_reimbursement' }
     end
+  end
+
+  def to_address(emp_id)
+    emp_id << "@thoughtworks.com"
   end
 end
--- spec/mailers/employee_mailer_spec.rb
+++ spec/mailers/employee_mailer_spec.rb
@@ -9,14 +9,13 @@ describe EmployeeMailer do
       travel = FactoryGirl.create(:outbound_travel, :place => 'UK', :departure_date => Time.parse('2011-10-01'))
       @travel_id = travel.id
       @expense_settlement = FactoryGirl.create(:expense_settlement, :empl_id =>
-                                               @employee_id, :cash_handovers => [],
-                                                 :expense_from => Time.parse('2011-10-01'),
-                                                :expense_to => Time.parse('2011-10-11'))
+          @employee_id, :cash_handovers => [],
+                                               :expense_from => Time.parse('2011-10-01'),
+                                               :expense_to => Time.parse('2011-10-11'))
       expense = {"report_id" => 123, "currency" => 'USD', "amount" => 1000, "conversion_rate" => 52.30, "local_currency_amount" => 52300}
       @expense_settlement.should_receive(:employee_detail).and_return(@employee_detail)
       @expense_settlement.should_receive(:get_consolidated_expenses).and_return([expense])
       @expense_settlement.should_receive(:get_forex_payments).and_return([forex])
-      # @expense_settlement.should_receive(:get_conversion_rate).and_return(expense["conversion_rate"])
       @expense_settlement.should_receive(:get_receivable_amount).at_least(2).times.and_return(52300)
     end
 
@@ -31,11 +30,11 @@ describe EmployeeMailer do
 
       it "should have the e-mail components properly set" do
         @email.to.size.should == 1
-        @email.to[0].should == @employee_detail.email
+        @email.to[0].should == "1@thoughtworks.com"
         @email.from.size.should == 1
         @email.from[0].should == ::Rails.application.config.email_sender
         @email.subject.should == 'test - ' +
-          EmployeeMailer::EXPENSE_SETTLEMENT_SUBJECT.sub('$start_date','01-Oct-2011').sub('$end_date','11-Oct-2011')
+            EmployeeMailer::EXPENSE_SETTLEMENT_SUBJECT.sub('$start_date', '01-Oct-2011').sub('$end_date', '11-Oct-2011')
         @email.body.should include(@expense_settlement.get_receivable_amount)
       end
 
