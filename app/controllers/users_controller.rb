bac9f51fe7f4e70cfb524b1db9cffc0bf4bdee72
Prasanna Venkatesan <pvenkat4ever@gmail.com>
2014-01-29 14:58:50 +0200

    Fixed the stron param for Rails 4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,16 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
       redirect_to(root_path)
     else
       render :action => 'new'
     end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:user_name, :password, :password_confirmation)
   end
 end
